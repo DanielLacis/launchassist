e3cc557a89432f39edbec09ec883ac20a1a2d61a
Daniel Lacis <danlacis@gmail.com>
2015-04-07 14:16:42 -0700

    added composite view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require underscore
 //= require backbone
+//= require composite_view
 //= require launch_assist
 //= require_tree ../templates
 //= require_tree ./models
