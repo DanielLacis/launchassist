e5ca087b137ced093fcd972d8ac0d4387c164d47
Daniel Lacis <danlacis@gmail.com>
2015-04-07 13:22:47 -0700

    added some api skeleton

---
+++ app/assets/javascripts/launch_assist.js
@@ -0,0 +1,13 @@
+window.LaunchAssist = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  LaunchAssist.initialize();
+});
---
+++ app/controllers/api/projects_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  class ProjectsController < ApiController
+    def show
+
+    end
+
+    def index
+
+    end
+  end
+end
