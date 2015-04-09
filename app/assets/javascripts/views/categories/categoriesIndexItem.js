6c2dcc08fd848888752505bbb40a87989318fea3
Daniel Lacis <danlacis@gmail.com>
2015-04-08 17:59:10 -0700

    sign up page, log in page, categories index improved

--- app/assets/javascripts/views/categories/categoriesIndexItem.js
+++ app/assets/javascripts/views/categories/categoriesIndexItem.js
@@ -1,15 +1,15 @@
 LaunchAssist.Views.CategoriesIndexItem = Backbone.View.extend({
   template: JST['categories/categories_index_item'],
-  className: 'category col-md-3',
-
+  className: 'category',
 
   initialize: function() {
     this.listenTo(this.model, 'sync', this.render);
   },
 
   render: function() {
-    var content = this.template({category: this.model, imageUrl: this.model.get('image_url')});
+    var content = this.template({category: this.model});
     this.$el.html(content);
+    this.$el.css('background-image', 'url(' + this.model.get('image_url') + ')');
     return this;
   }
 });
