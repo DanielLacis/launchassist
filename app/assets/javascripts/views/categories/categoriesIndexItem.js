96763825c404b7345b6c851b396a45b4ba2f0dcf
Daniel Lacis <danlacis@gmail.com>
2015-04-16 14:30:12 -0700

    not happy with category index

--- app/assets/javascripts/views/categories/categoriesIndexItem.js
+++ app/assets/javascripts/views/categories/categoriesIndexItem.js
@@ -1,9 +1,8 @@
 LaunchAssist.Views.CategoriesIndexItem = Backbone.View.extend({
   template: JST['categories/categories_index_item'],
-  className: 'category',
+  className: 'category col-md-3',
 
   events: {
-    'mouseover': 'changeCursor',
     'click': 'linkToCategory'
   },
 
@@ -14,16 +13,11 @@ LaunchAssist.Views.CategoriesIndexItem = Backbone.View.extend({
   render: function() {
     var content = this.template({category: this.model});
     this.$el.html(content);
-    this.$el.css('background-image', 'url(' + this.model.escape('image_url_index') + ')');
     return this;
   },
 
   linkToCategory: function(event) {
     event.preventDefault();
     Backbone.history.navigate('#categories/' + this.model.get('id'), {trigger: true});
-  },
-
-  changeCursor: function(event) {
-    $(event.currentTarget).css('cursor', 'pointer');
   }
 });
